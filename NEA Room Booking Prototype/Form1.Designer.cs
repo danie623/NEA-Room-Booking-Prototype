76ba118bfe5b2845b28d504c5d72dcb260dfe2fa
244257 <244257@PC107005>
2025-09-17 15:56:23 +0100

    Add project files.

---
+++ NEA Room Booking Prototype/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace NEA_Room_Booking_Prototype
+{
+	partial class BookingScreen
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// BookingScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1898, 1024);
+			this.MaximumSize = new System.Drawing.Size(1920, 1080);
+			this.MinimumSize = new System.Drawing.Size(1920, 1080);
+			this.Name = "BookingScreen";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.BookingScreen_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
---
+++ NEA Room Booking Prototype/SQLQUEREYS/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿SELECT * FROM TagAssign;
+SELECT * FROM Tags;
+SELECT * FROM Rooms;
+
+SELECT *
+FROM Rooms r, Tags t, TagAssign a
+WHERE r.RoomID = a.RoomID AND a.TagID = t.TagID AND t.Tag = 'Aircon / Ceiling fans';
